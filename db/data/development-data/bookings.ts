ac16af3713333750ff3b60aa56f26e32d72210f2
chrisrduckworth <c_duckworth@hotmail.com>
2024-04-05 22:22:13 +0100

    add dev data

--- db/data/development-data/bookings.ts
+++ db/data/development-data/bookings.ts
@@ -1,1 +1,1007 @@
-module.exports = []
+import { Booking } from "../interfaces.js"
+
+const bookings: Booking[] = [
+  {
+    "screening_id": 18,
+    "email": "Kayleigh14@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 30,
+    "email": "Marilie11@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 15,
+    "email": "Ottis.Bosco@gmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 10,
+    "email": "Alana_Reinger62@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 30,
+    "email": "Carol.Kub87@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 17,
+    "email": "Madison46@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 23,
+    "email": "Cleora98@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 29,
+    "email": "Forrest90@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 6,
+    "email": "Johnnie75@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 11,
+    "email": "Walter.Bartoletti27@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 18,
+    "email": "Davin_Borer30@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 20,
+    "email": "Shanny_Rice-Dooley17@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 4,
+    "email": "Dolly65@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 6,
+    "email": "Presley.Hayes@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 17,
+    "email": "Marcos58@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 17,
+    "email": "Delmer_Schamberger-Yost95@hotmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 19,
+    "email": "Francis38@gmail.com",
+    "charge": 200
+  },
+  {
+    "screening_id": 23,
+    "email": "Keegan_Conn@yahoo.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 8,
+    "email": "Torey_Schultz56@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 21,
+    "email": "Tomas_Tillman@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 5,
+    "email": "Stacey17@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 14,
+    "email": "Verna_Klein@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 15,
+    "email": "Francesco_Nolan21@hotmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 22,
+    "email": "Rosalinda_Parisian52@gmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 7,
+    "email": "Estrella.DuBuque@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 18,
+    "email": "Elinor_Casper-Schoen@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 17,
+    "email": "Marta_Lowe98@hotmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 20,
+    "email": "Edyth24@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 1,
+    "email": "Dillan_Hessel@yahoo.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 11,
+    "email": "Edwina.Monahan60@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 4,
+    "email": "Evelyn.Cronin@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 10,
+    "email": "Emilie.Pollich@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 18,
+    "email": "Tomasa3@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 29,
+    "email": "Major.Bartell-OKon@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 7,
+    "email": "Arjun_OKon@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 4,
+    "email": "Shayna_Kertzmann77@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 27,
+    "email": "Nikki.Kiehn-Borer@yahoo.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 24,
+    "email": "Arden.Wilkinson-VonRueden@yahoo.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 25,
+    "email": "Lizzie_Gerlach72@gmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 23,
+    "email": "Margie.Rogahn@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 12,
+    "email": "Dominique0@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 6,
+    "email": "Rosa15@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 5,
+    "email": "Dewayne11@yahoo.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 15,
+    "email": "Garry_Lowe3@yahoo.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 21,
+    "email": "Rudolph50@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 9,
+    "email": "Lisandro.Koch@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 24,
+    "email": "Santino.Beier@yahoo.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 25,
+    "email": "Jaiden.Glover32@gmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 28,
+    "email": "Taryn.Weber70@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 26,
+    "email": "Ellsworth38@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 14,
+    "email": "Jack_Monahan@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 12,
+    "email": "Eli_Schamberger55@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 30,
+    "email": "Zola.Bins48@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 2,
+    "email": "Kennedi96@yahoo.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 2,
+    "email": "Braeden_Lindgren@hotmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 26,
+    "email": "Consuelo.Murazik@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 14,
+    "email": "Desiree.Gutkowski@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 5,
+    "email": "Ambrose.Sipes-Beahan29@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 10,
+    "email": "Kathleen63@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 16,
+    "email": "Chadd6@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 15,
+    "email": "Margaret.Bogan2@hotmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 4,
+    "email": "Leonie7@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 14,
+    "email": "Gardner_Wisozk@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 25,
+    "email": "Charles.Hermann10@yahoo.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 18,
+    "email": "Maddison.Johnson@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 23,
+    "email": "Antonette.Hammes-Williamson8@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 5,
+    "email": "Jed.Stark7@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 5,
+    "email": "Hanna_Kiehn-Bechtelar40@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 17,
+    "email": "Selmer_Lubowitz-Harris62@gmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 20,
+    "email": "Enrico59@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 26,
+    "email": "Athena.Moen@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 3,
+    "email": "Avis.Wiza@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 11,
+    "email": "Hillard_Okuneva37@hotmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 8,
+    "email": "Karson56@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 22,
+    "email": "Ulices.Stracke23@yahoo.com",
+    "charge": 208
+  },
+  {
+    "screening_id": 22,
+    "email": "Neva5@hotmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 16,
+    "email": "Keaton_Macejkovic51@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 1,
+    "email": "Sedrick.Kub@hotmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 18,
+    "email": "Brooke1@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 19,
+    "email": "Bethel68@yahoo.com",
+    "charge": 200
+  },
+  {
+    "screening_id": 13,
+    "email": "Grayson.Larkin@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 12,
+    "email": "Roxane.Herman@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 1,
+    "email": "Holden.Sanford@yahoo.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 26,
+    "email": "Enrico.Ferry-Schmidt@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 2,
+    "email": "Dillan.Stroman@hotmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 23,
+    "email": "Ashton_Olson@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 7,
+    "email": "Evelyn.Predovic@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 10,
+    "email": "Adell_Harris16@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 3,
+    "email": "Emma.Baumbach@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 20,
+    "email": "Rashawn.Wisozk85@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 25,
+    "email": "Doris_Schulist@hotmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 13,
+    "email": "Emmanuelle7@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 24,
+    "email": "Macie_Miller@gmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 8,
+    "email": "Jermey35@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 7,
+    "email": "Garfield92@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 11,
+    "email": "Ward48@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 22,
+    "email": "Lina30@yahoo.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 7,
+    "email": "Erick_Nolan60@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 27,
+    "email": "Noelia96@gmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 11,
+    "email": "Pearline_Turcotte-Schultz@hotmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 10,
+    "email": "Johanna34@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 21,
+    "email": "Kolby.Buckridge84@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 22,
+    "email": "Sean.Harber@yahoo.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 6,
+    "email": "Carmel.Donnelly76@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 18,
+    "email": "Israel.Kunde62@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 21,
+    "email": "Anthony_Sipes@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 30,
+    "email": "Melisa.Emard@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 5,
+    "email": "Bernie_Stamm24@yahoo.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 13,
+    "email": "Dimitri.Cremin@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 11,
+    "email": "Bo67@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 14,
+    "email": "Elwyn_Emmerich31@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 30,
+    "email": "Fabiola69@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 2,
+    "email": "Candice.OConner@gmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 30,
+    "email": "Aron.Beahan63@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 19,
+    "email": "Geovanni.Mertz33@hotmail.com",
+    "charge": 200
+  },
+  {
+    "screening_id": 8,
+    "email": "Leonora_Franey46@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 16,
+    "email": "Norene_Schultz@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 6,
+    "email": "Sherman.Christiansen@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 20,
+    "email": "Laila58@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 18,
+    "email": "Abbie_Grimes-Nienow@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 10,
+    "email": "Lamar32@yahoo.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 8,
+    "email": "Marlen_DAmore@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 26,
+    "email": "Jettie.Lueilwitz@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 2,
+    "email": "Berneice.Jerde-Langworth65@gmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 4,
+    "email": "Alexandrea.Daugherty@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 23,
+    "email": "Marcellus.Roberts0@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 6,
+    "email": "Keven_Kuvalis@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 1,
+    "email": "Kellie49@gmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 15,
+    "email": "Genesis97@gmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 26,
+    "email": "Annetta23@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 26,
+    "email": "Dax_Nader@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 5,
+    "email": "Ashlynn_Huels69@hotmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 21,
+    "email": "Serena_Runolfsdottir@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 7,
+    "email": "Reinhold_Stracke82@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 14,
+    "email": "Johan69@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 24,
+    "email": "Zoey13@hotmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 29,
+    "email": "Steve_Monahan@yahoo.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 12,
+    "email": "Elvera.Sipes1@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 21,
+    "email": "Larue.Mayert@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 30,
+    "email": "Bridie.Boyle-MacGyver94@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 28,
+    "email": "Junius.Schmeler73@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 17,
+    "email": "Deondre_Pacocha@hotmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 14,
+    "email": "Turner_Collier@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 23,
+    "email": "Ila28@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 3,
+    "email": "Annabel_Herman21@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 11,
+    "email": "Patricia10@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 28,
+    "email": "Lourdes.Heller48@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 23,
+    "email": "Vicente99@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 9,
+    "email": "Camylle_Monahan76@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 4,
+    "email": "Johanna_Feil-Block46@gmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 22,
+    "email": "Rebeka.Lehner@yahoo.com",
+    "charge": 797
+  },
+  {
+    "screening_id": 2,
+    "email": "Lenore45@yahoo.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 1,
+    "email": "Janessa63@hotmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 3,
+    "email": "Emile40@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 29,
+    "email": "Gregoria_Wyman22@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 7,
+    "email": "Dessie7@hotmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 26,
+    "email": "Keegan_Berge62@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 21,
+    "email": "Gunner_McDermott@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 3,
+    "email": "Audreanne_Powlowski2@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 26,
+    "email": "Frankie_Dach@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 23,
+    "email": "Anderson_Dare@hotmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 9,
+    "email": "Lea_Farrell9@hotmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 15,
+    "email": "Hugh.Barrows@gmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 27,
+    "email": "Frieda_Yundt43@gmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 18,
+    "email": "Marielle_Weimann64@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 24,
+    "email": "Grover_Spencer58@gmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 27,
+    "email": "Philip.Terry-Weissnat@hotmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 3,
+    "email": "Petra_Predovic12@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 9,
+    "email": "Wendy_Rowe@hotmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 6,
+    "email": "Desmond.Bednar@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 18,
+    "email": "Nickolas22@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 7,
+    "email": "Joany_Boehm20@gmail.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 18,
+    "email": "Ella_Williamson@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 7,
+    "email": "Tessie.Klein53@yahoo.com",
+    "charge": 500
+  },
+  {
+    "screening_id": 21,
+    "email": "Bell_Deckow@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 18,
+    "email": "Fabian.Kilback@yahoo.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 2,
+    "email": "Filiberto_Strosin18@hotmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 29,
+    "email": "Isaac_Haley78@yahoo.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 17,
+    "email": "Ernest77@hotmail.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 10,
+    "email": "Summer_Pfannerstill@gmail.com",
+    "charge": 900
+  },
+  {
+    "screening_id": 24,
+    "email": "Bette27@gmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 12,
+    "email": "Melisa.Balistreri@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 18,
+    "email": "Karli_Macejkovic42@gmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 24,
+    "email": "Trycia_Treutel@gmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 18,
+    "email": "Ellis.Grant@hotmail.com",
+    "charge": 300
+  },
+  {
+    "screening_id": 5,
+    "email": "Jimmy.Harber-Dickinson32@yahoo.com",
+    "charge": 800
+  },
+  {
+    "screening_id": 28,
+    "email": "Sarina_Larson@hotmail.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 19,
+    "email": "Elenora54@hotmail.com",
+    "charge": 200
+  },
+  {
+    "screening_id": 9,
+    "email": "Winfield.Hermiston@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 25,
+    "email": "Keegan_Cummings@gmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 9,
+    "email": "Devante29@gmail.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 4,
+    "email": "Johnson72@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 22,
+    "email": "Winnifred7@hotmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 13,
+    "email": "Carole_Daniel74@yahoo.com",
+    "charge": 700
+  },
+  {
+    "screening_id": 30,
+    "email": "Coralie_Schamberger68@yahoo.com",
+    "charge": 100
+  },
+  {
+    "screening_id": 15,
+    "email": "Ken.Konopelski43@yahoo.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 1,
+    "email": "Camila38@gmail.com",
+    "charge": 0
+  },
+  {
+    "screening_id": 24,
+    "email": "Emmanuelle_Morissette@hotmail.com",
+    "charge": 400
+  },
+  {
+    "screening_id": 15,
+    "email": "Eden_Pouros82@gmail.com",
+    "charge": 600
+  },
+  {
+    "screening_id": 19,
+    "email": "Lenora_Hamill@hotmail.com",
+    "charge": 200
+  }
+]
+
+
+module.exports = bookings
