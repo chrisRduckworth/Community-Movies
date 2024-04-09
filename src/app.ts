ff76f1f130d1161151265afcb6673661d079b1e6
chrisrduckworth <c_duckworth@hotmail.com>
2024-04-09 22:39:17 +0100

    add error handling for bad screening id

--- src/app.ts
+++ src/app.ts
@@ -20,14 +20,24 @@ app.get("/api/screenings/:screening_id", getScreeningDetails);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err.status) {
-    return res.status(err.status).send({ msg: err.msg });
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
   }
 
-  next(err);
 });
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.log(err);
+  const badReqCodes = ["22P02"];
+  if (badReqCodes.includes(err.code)) {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err)
+  }
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err)
   res.sendStatus(500);
 });
 
